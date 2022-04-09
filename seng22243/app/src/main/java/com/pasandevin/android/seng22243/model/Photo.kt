874ce5e03441eb6a60a6ce816c763bd1c3ac94ac
pasandevin <jayawardene.pasandevin@gmail.com>
2022-04-09 12:25:16 +0530

    embedded classes

--- seng22243/app/src/main/java/com/pasandevin/android/seng22243/model/Photo.kt
+++ seng22243/app/src/main/java/com/pasandevin/android/seng22243/model/Photo.kt
@@ -1,14 +1,21 @@
 //Recycler View, Database//
 package com.pasandevin.android.seng22243.model
 
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+
+data class Metadata(
+    var size: String,
+    var owner: String
+)
 
 @Entity(tableName = "photo")
 data class Photo (
     @PrimaryKey var id: String,
     var title: String,
     var thumbnailUrl: String,
-    var url: String
+    var url: String,
+    @Embedded var metadata: Metadata?
     )
 //Recycler View, Database//
